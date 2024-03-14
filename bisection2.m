dc419524a68b89975829e79f74346cbdbef04747
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-03-14 11:13:40 -0700

    Add files via upload

---
+++ bisection2.m
@@ -0,0 +1,30 @@
+% soheilsolim
+clc;
+clear;
+close all;
+format long
+% bisection
+ f = @(x) (x+cos(x));
+ a = 0 ;
+ b = -1 ;
+x = (a+b)/2;
+
+i = 1 ;
+for c=1:20
+    x = (a+b)/2;
+    % if x&a<0
+    if f(a)*f(x) < 0
+        b = x ;
+    % if x&b<0    
+    else
+        a = x ;   
+    end
+
+    % answer table 
+    STR = ['n = ', num2str(i), ', x = ' ,num2str(x), ', f(x) = ' ...
+    ,num2str(f(x))];
+    disp(STR)
+    i=i+1;
+    
+    
+end
